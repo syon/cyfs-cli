d0af0e9daca82ff12cb8498b3afaeecbfebe0623
syon <syon.xiii@gmail.com>
2018-05-15 22:59:10 +0900

    :memo: TODO

--- src/bridge.ts
+++ src/bridge.ts
@@ -4,6 +4,7 @@ import * as cyfs from "cyfs"
 import { Order } from "./order"
 
 export default (args: { file: string }, flags: object): String => {
+  // TODO: auto detect encofing
   const buf = fs.readFileSync(args.file, "utf-8")
   const order: Order = <Order>yaml.safeLoad(buf)
   const list = cyfs(order, flags)
